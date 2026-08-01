3d9c22159a78c185d23044b2883a40386f47cace
StudyPet <studypet@local.dev>
2026-08-01 15:08:30 +0800

    chore: remove temp file

--- _c.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const src = fs.readFileSync('src/seed.ts', 'utf8');
-const total = [...src.matchAll(/subject:\s*['"]/g)].length;
-const imports = [...src.matchAll(/source: 'import'/g)].length;
-console.log('总题数:', total, '| 真题:', imports);
